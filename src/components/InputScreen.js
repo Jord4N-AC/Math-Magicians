0fd784200902809c66c8c52b09bf70d92029a05d
Jordan <camiloarmijosantifonamusic@gmail.com>
2023-03-23 08:45:09 -0500

    Add InputScreen.js, create InputScreen component for displaying calculations and result, export

---
+++ src/components/InputScreen.js
@@ -0,0 +1,8 @@
+const InputScreen = () => (
+  <div>
+    <input type="text" placeholder="0 + 0" disabled />
+    <input type="text" placeholder="0" disabled />
+  </div>
+);
+
+export default InputScreen;
