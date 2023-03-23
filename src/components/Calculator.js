c7aed84faee7d89d9352f9c23328fdef9a4a0f4c
Jordan <camiloarmijosantifonamusic@gmail.com>
2023-03-23 11:48:10 -0500

    Create Calculator component, import and export

---
+++ src/components/Calculator.js
@@ -0,0 +1,11 @@
+import InputScreen from './InputScreen';
+import Buttons from './Buttons';
+
+const Calculator = () => (
+  <div>
+    <InputScreen />
+    <Buttons />
+  </div>
+);
+
+export default Calculator;
