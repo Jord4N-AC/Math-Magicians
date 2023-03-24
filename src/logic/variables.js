57826c25d4f3c7a1eb833067fc23a121e94963c4
Jordan <camiloarmijosantifonamusic@gmail.com>
2023-03-24 17:29:15 -0500

    Create logic folder, add variables.js, move variables from Buttons.js

---
+++ src/logic/variables.js
@@ -0,0 +1,14 @@
+const calcButtons = [
+  'AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4',
+  '5', '6', '-', '1', '2', '3', '+', '0', '.', '=',
+];
+
+const calcButtonsTitle = [
+  'All Clear (clears all values on screen)',
+  'changes sign of a number, ie: 5 = -5 or -5 = 5, useful for calculations with negative numbers',
+  'calculates percentages ei: 2% = 0.02',
+  '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0',
+  '. (decimal separator)', '=',
+];
+
+export { calcButtons, calcButtonsTitle };
