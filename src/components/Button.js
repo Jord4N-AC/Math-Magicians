a813a45bd56c5ffbafc8577c316e5063e6e4b12d
Jordan <camiloarmijosantifonamusic@gmail.com>
2023-03-24 18:34:04 -0500

    Add className to Button component to style

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
+import '../assets/Button.css';
 
-const Button = ({ content, title }) => <button type="button" title={title}>{content}</button>;
+const Button = ({ content, title, className }) => <button type="button" className={className} title={title}>{content}</button>;
 
 Button.propTypes = {
   content: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
 };
 
 export default Button;
